95235eb4cbb00e0a4cf2b73709c110b67b8d7eda
Beyza <94379225+0xbeyza@users.noreply.github.com>
2024-05-26 02:14:37 +0300

    Update run.sh

--- packages/backend/run.sh
+++ packages/backend/run.sh
@@ -1,3 +1,13 @@
 echo "Process Started!"
+git clone https://github.com/itublockchain/ETHBerlin-1
+echo "Cloned!"
+mv ETHBerlin-1 chimera
+echo "Renamed"
+cd chimera
+echo "cd chimera"
+cd packages
+echo "cd packages"
+cd backend
+echo "cd backend"
 npm run initialize_backend
-npm run start
+npm run start_backend
