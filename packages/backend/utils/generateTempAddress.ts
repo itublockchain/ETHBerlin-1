5a5874f0a5c51011752da59eb8e64f16ffbbda12
0xbeyza <mbeyzaoral9@gmail.com>
2024-05-25 13:28:47 +0300

    temp adresses

---
+++ packages/backend/utils/generateTempAddress.ts
@@ -0,0 +1,8 @@
+import {generateMnemonic,EthHdWallet} from "eth-hd-wallet"
+
+export async function generateTempAddresses(nonce:Number, mnemonic: String) {
+    const wallet = EthHdWallet.fromMnemonic(mnemonic)
+    return wallet.generateAddresses(nonce)
+    
+
+}
