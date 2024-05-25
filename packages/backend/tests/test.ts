076f086ae60891099ca0f4c906d2763f95652f1d
uok825 <omerleroyunda@gmail.com>
2024-05-25 11:49:52 +0200

    feat: test for addreses

---
+++ packages/backend/tests/test.ts
@@ -0,0 +1,12 @@
+const { generateMnemonic, EthHdWallet } = require("eth-hd-wallet");
+
+const mnemonic = generateMnemonic();
+console.log(mnemonic);
+const randomWallet = EthHdWallet.fromMnemonic(mnemonic);
+console.log(randomWallet.generateAddresses(5));
+
+const testMnemonic =
+  "ugly pelican fatal snake aerobic fabric spoon iron wide upgrade tornado during";
+
+const wallet = EthHdWallet.fromMnemonic(testMnemonic);
+console.log(wallet.generateAddresses(5));
