95a9fe4fd9379b8fd2fe35433a072b8712c6ba12
Beyza <94379225+0xbeyza@users.noreply.github.com>
2024-05-26 03:10:35 +0300

    server.js fix

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors')
 const app = express();
-app.use(cors());X
+app.use(cors());
 const port = 3000;
 const { createMainWallet } = require("./utils/createMainWallet");
 const { generateTempAddresses } = require("./utils/generateTempAddresses");
